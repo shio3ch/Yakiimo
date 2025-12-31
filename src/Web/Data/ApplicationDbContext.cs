d473a8fac0b5d1991454745cb7ee4c411fb1300f
shio3ch <rrrrse13@gmail.com>
2026-01-01 02:03:18 +0900

    initial commit

---
+++ src/Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Web.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
